7a87c91cd0210ac238e1fd2a285023b2a2594dfa
parkgichan <rl0425@naver.com>
2021-05-27 22:39:12 +0900

    q

--- Algorithm-Programming/Programmers_site_question/question9.js
+++ Algorithm-Programming/Programmers_site_question/question9.js
@@ -1,18 +1,26 @@
+let answers = [1,2,3,4,5];
+let answers2 = [1,3,2,4,2];
+
+solution(answers);
+
+
 function solution(answers) {
-    var answer = [];
-    var a1 = [1, 2, 3, 4, 5];
-    var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let answer = [];
+    let a1 = [1, 2, 3, 4, 5];
+    let a2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    let a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-    var a1c = answers.filter((a,i)=> a === a1[i%a1.length]).length;
-    var a2c = answers.filter((a,i)=> a === a2[i%a2.length]).length;
-    var a3c = answers.filter((a,i)=> a === a3[i%a3.length]).length;
-    var max = Math.max(a1c,a2c,a3c);
+    let n1 = answers.filter((a,i) => a === a1[i%a1.length]).length;
+    let n2 = answers.filter((a,i) => a === a2[i%a2.length]).length;
+    let n3 = answers.filter((a,i) => a === a3[i%a3.length]).length;
+    
+    let max = Math.max(n1,n2,n3);
+    if(max === n1) answer.push(1);
+    if(max === n2) answer.push(2);
+    if(max === n3) answer.push(3);
+    
 
-    if (a1c === max) {answer.push(1)};
-    if (a2c === max) {answer.push(2)};
-    if (a3c === max) {answer.push(3)};
-
-
+    console.log(answer);
     return answer;
+
 }
